4be057aa0f533be0e232b782ea71a6e7aff1beec
Vadim Manushin <vadimmanushin@yandex.ru>
2023-04-14 01:14:47 +0300

    pagination added

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,30 +1,43 @@
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import ProductCard, { ProductCardPropsType } from '../components/product-card/ProductCard';
 import './MainPage.scss';
 import products from '../../sources/assets/products.json';
 
-const props = {
-    "type": "simple",
-    "id": 9,
-    "sku": "s9",
-    "title": "Product 9",
-    "regular_price": {
-      "currency": "USD",
-      "value": 53.40
-    },
-    "image": "/images/9.png",
-    "brand": 2
-  }
-
 const MainPage = ()=> {
-  const productsArray = products.map((product)=>{
-    return <ProductCard key={product.id} {...product} />
-  })
+  const [ cards, setCards ] = useState<Array<ProductCardPropsType>>(products.sort(
+    (a, b) => a.title > b.title ? 1 : -1));
+  const PRODUCTS_PER_PAGE = 6;
+  const [ itemOffset, setItemOffset ] = useState(0);
+  const endOffset = itemOffset + PRODUCTS_PER_PAGE;
+  const currentItems = cards.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(cards.length / PRODUCTS_PER_PAGE);
+
+  const handlePaginationClick = (event:{selected:number}) => {
+    const newOffset = (event.selected * PRODUCTS_PER_PAGE) % cards.length;
+    setItemOffset(newOffset);
+  };
 
   return (
-    <div className="App">
+    <main className="main-page">
       <h1>Тестовое задание интернет магазин</h1>
-      {productsArray}
-    </div>
+      {
+        currentItems.map((product) => {
+          return <ProductCard key={product.id} {...product} />
+        })
+      }
+      <div className="main-page__pagination">
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePaginationClick}
+          pageRangeDisplayed={PRODUCTS_PER_PAGE}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={() => null}
+        />
+      </div>
+    </main>
   )
 }
 
